2ed32ddb66604bac92048708208e3e46809bfdf7
joshuamathia <joshuamathia@pnch.xyz>
2025-11-13 01:11:51 -0800

    Started a new file about control flow

--- control-flow/01.py
+++ control-flow/01.py
@@ -1,10 +1,9 @@
 # Example 1: Checking a condition
 num = 15
 
-if num  > 10:
+if num > 10:
     print("Positive Number")
 elif num == 0:
-    print("Number = 0") 
+    print("Number = 0")
 else:
     print("Negative Number")
-    
